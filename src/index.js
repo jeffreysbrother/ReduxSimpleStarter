81f59fe10ac47c3c8fca8615444f29ed762b1e5a
James Cool <james.cool@thecontrolgroup.com>
2017-09-25 15:48:28 -0700

    add unique key

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyA8e9vk-gXKsvt5CXE7ABZQJYOgdeUSMuk';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
-	console.log(data);
-});
+
 
 // downward data-flow: only the most parent component should be responsible for fetching data
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-};
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				{/* passing props */}
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
