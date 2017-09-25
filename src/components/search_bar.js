d62122c4e260f3de037ff655010b73e478a7c06a
James Cool <james.cool@thecontrolgroup.com>
2017-09-25 15:08:07 -0700

    setState"

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
+// each class-based component has its own state object
+// when the state is changed, the component immediately re-renders
+// and forces all children to re-render as well
 class SearchBar extends Component {
-	render() {
-		return <input onChange={this.onInputChange} />;
+	constructor(props) {
+		super(props);
+		// initialization of state
+		this.state = { term: '' };
 	}
-
-	onInputChange(event) {
-		console.log(event.target.value)
+	render() {
+		return (
+			<div>
+				<input onChange={event => this.setState({ term: event.target.value })} />
+				Value of input: {this.state.term}
+			</div>
+		);
 	}
 }
 
