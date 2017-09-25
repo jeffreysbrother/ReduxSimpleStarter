4e88aa7c664962338cf6195e5df024e79d6d6f81
James Cool <james.cool@thecontrolgroup.com>
2017-09-25 15:22:40 -0700

    get API data with YTSearch plugin

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,8 +12,10 @@ class SearchBar extends Component {
 	render() {
 		return (
 			<div>
-				<input onChange={event => this.setState({ term: event.target.value })} />
-				Value of input: {this.state.term}
+				{/* a controlled component has its value set by state */}
+				<input
+					value={this.state.term}
+					onChange={event => this.setState({ term: event.target.value })} />
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyA8e9vk-gXKsvt5CXE7ABZQJYOgdeUSMuk';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
+	console.log(data);
+});
+
+// downward data-flow: only the most parent component should be responsible for fetching data
 const App = () => {
 	return (
 		<div>
